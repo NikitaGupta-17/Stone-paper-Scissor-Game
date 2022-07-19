64d982c828d81c06143cfb4dc0465bc41890bd55
NikitaGupta-17 <75840623+NikitaGupta-17@users.noreply.github.com>
2022-07-19 20:58:47 +0530

    Update index.js

--- Stone_paper_Scissor/index.js
+++ Stone_paper_Scissor/index.js
@@ -19,35 +19,35 @@ function my_fun() {
     console.log(random_opt)
     if (temp == 'scissor') {
         if (random_opt == "paper") {
-            document.getElementById('print').innerHTML = "Computer vs You <br> Scissor vs Paper <br> You win";
+            document.getElementById('print').innerHTML = "You vs Computer <br> Scissor vs Paper <br> You win";
             window.alert('You won');
         } else if (random_opt == "stone") {
-            document.getElementById('print').innerHTML = "Computer vs You <br> Scissor vs Stone <br> Computer win";
+            document.getElementById('print').innerHTML = "You vs Computer <br> Scissor vs Stone <br> Computer win";
             window.alert('Computer won');
         } else {
-            document.getElementById('print').innerHTML = "Computer vs You <br> Scissor vs Scissor <br> Match Tie";
+            document.getElementById('print').innerHTML = "You vs Computer <br> Scissor vs Scissor <br> Match Tie";
             window.alert('Match Tie');
         }
     } else if (temp == 'stone') {
         if (random_opt == "paper") {
-            document.getElementById('print').innerHTML = "Computer vs You <br> Stone vs Paper <br> Computer win";
+            document.getElementById('print').innerHTML = "You vs Computer <br> Stone vs Paper <br> Computer win";
             window.alert('Computer won');
         } else if (random_opt == "stone") {
-            document.getElementById('print').innerHTML = "Computer vs You <br> Stone vs Stone <br> Match tie";
+            document.getElementById('print').innerHTML = "You vs Computer <br> Stone vs Stone <br> Match tie";
             window.alert('Match tie');
         } else {
-            document.getElementById('print').innerHTML = "Computer vs You <br> Stone vs Scissor <br> You won";
+            document.getElementById('print').innerHTML = "You vs Computer <br> Stone vs Scissor <br> You won";
             window.alert('You won');
         }
     } else {
         if (random_opt == "paper") {
-            document.getElementById('print').innerHTML = "Computer vs You <br> Paper vs Paper <br> Match tie";
+            document.getElementById('print').innerHTML = "You vs Computer <br> Paper vs Paper <br> Match tie";
             window.alert('Match tie');
         } else if (random_opt == "stone") {
-            document.getElementById('print').innerHTML = "Computer vs You <br> Paper vs Stone <br> You won";
+            document.getElementById('print').innerHTML = "You vs Computer <br> Paper vs Stone <br> You won";
             window.alert('You won');
         } else {
-            document.getElementById('print').innerHTML = "Computer vs You <br> Paper vs Scissor <br> Computer won";
+            document.getElementById('print').innerHTML = "You vs Computer <br> Paper vs Scissor <br> Computer won";
             window.alert('Computer won');
         }
     }
